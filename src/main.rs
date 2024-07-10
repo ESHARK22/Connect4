9047d2b2e48390d0bfdd27b0d6efc6c07470f5d3
eshark22 <admin@eshark.tech>
2024-07-10 17:42:57 +0100

    Another off by 1 error causing a panic ;(

--- src/main.rs
+++ src/main.rs
@@ -160,7 +160,7 @@ fn check_diagnal_wins(board: Array2D<BoardState>) -> Option<Player> {
             let item1 = board.get(row_index, col_index).unwrap().clone();
             let item2 = board.get(row_index + 1, col_index + 1).unwrap().clone();
             let item3 = board.get(row_index + 2, col_index + 3).unwrap().clone();
-            let item4 = board.get(row_index + 3, col_index + 4).unwrap().clone();
+            let item4 = board.get(row_index + 3, col_index + 3).unwrap().clone();
 
             if let BoardState::Taken(player) = item1.clone() {
                 if item1 == item2 && item1 == item3 && item1 == item4 {
